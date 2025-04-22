86cef5831d889f7988c092761a985f5249a92393
chae0Park <cypark022@gmail.com>
2025-04-22 23:59:14 +0900

    searchfrequency schema edited

--- back/model/SearchFrequency.js
+++ back/model/SearchFrequency.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-const searchFrequencySchema = new mongoose.Schema({
-    term: { type: String, required: true, unique: true },
-    count: { type: Number, required: true, default: 1 }
-});
+const searchFrequencySchema = new mongoose.Schema(
+    {
+        term: { type: String, required: true, unique: true },
+        count: { type: Number, required: true, default: 1 }
+}
+);
 
-const SearchFrequency = mongoose.model('SearchFrequency', searchFrequencySchema);
+const SearchFrequency = mongoose.model('SearchFrequency', searchFrequencySchema, 'searchFrequency');
 module.exports = SearchFrequency;
